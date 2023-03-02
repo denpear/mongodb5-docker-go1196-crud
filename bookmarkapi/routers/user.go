d8f01c908dffeb32a93c1a10f9ed61737d38cd20
Denis Grukhin <79031572161@ya.ru>
2023-03-02 17:12:35 +0300

    Fixing Users controller bug in the router

--- bookmarkapi/routers/user.go
+++ bookmarkapi/routers/user.go
@@ -8,7 +8,7 @@ import (
 
 // SetUserRoutes registers routes for user entity
 func SetUserRoutes(router *mux.Router) *mux.Router {
-	router.HandleFunc("/users", controllers.Register).Methods("POST")
+	router.HandleFunc("/users/register", controllers.Register).Methods("POST")
 	router.HandleFunc("/users/login", controllers.Login).Methods("POST")
 	return router
 }
